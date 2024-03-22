9ddcbbc4b9de668eb7408f5a43159a7571cc0af1
PJMonticoli <pablo_monticoli@hotmail.com>
2024-03-22 11:05:50 -0300

    agrego componente login y botones al sidenav

--- helphubapp/src/app/app.routes.ts
+++ helphubapp/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { VectorsTableComponent } from './components/vectors-table/vectors-table.component';
 import { UserRegisterComponent } from './components/users/user-register/user-register.component';
+import { LoginComponent } from './components/users/login/login.component';
 
 export const routes: Routes = [
     {path : '', component: HomeComponent},
     {path : 'vectors_table',component : VectorsTableComponent},
-    {path : 'user-register', component : UserRegisterComponent}
+    {path : 'user-register', component : UserRegisterComponent},
+    {path : 'user-login', component : LoginComponent}
 ];
---
+++ helphubapp/src/app/components/users/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SupabaseService } from '../../../services/supabase.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { SidenavComponent } from '../../sidenav/sidenav.component';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [SidenavComponent,ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  private subscription = new Subscription();
+  formulario: FormGroup;
+  fieldTextType: boolean = false;
+  @Input() modalId: number = 1;
+  mostrarCheckboxTerminos: boolean = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private servicioSupabase : SupabaseService,
+    private router : Router,
+    private toastr : ToastrService
+  ) {
+    this.formulario = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  ngOnInit(): void {}
+
+
+  get controlEmail(): FormControl {
+    return this.formulario.controls['email'] as FormControl;
+  }
+
+  get controlPassword() : FormControl{
+    return this.formulario.controls['password'] as FormControl;
+  }
+
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
+
+  iniciarSesion() {
+    if (this.formulario.valid) {
+      const email = this.formulario.value.email;
+      const password = this.formulario.value.password;
+
+      this.servicioSupabase.signUp(email, password).subscribe({
+        next: () => {
+          this.toastr.success("Inicio Sesión con exitó");
+          this.router.navigate(['']);
+        },
+        error: (err) => {
+          this.toastr.error("Error al inicar sesión, revise y complete todos los campos!");
+          console.log(err);
+        }
+      });
+    }
+  }
+}
