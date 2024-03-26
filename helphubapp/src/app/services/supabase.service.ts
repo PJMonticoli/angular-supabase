13f34f9bcd88b5e2c4e65c4c6768ab009465266f
PJMonticoli <pablo_monticoli@hotmail.com>
2024-03-26 10:05:14 -0300

    agrego al service logica para insertar y obtener user id

--- helphubapp/src/app/services/supabase.service.ts
+++ helphubapp/src/app/services/supabase.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 
 
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { Observable } from 'rxjs';
 
 
 
@@ -12,7 +13,7 @@ import { SupabaseClient, createClient } from '@supabase/supabase-js';
 export class SupabaseService {
   apiUrl : string = 'https://cinxvmcoxltagqjllntb.supabase.co';
   apiKey : string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpbnh2bWNveGx0YWdxamxsbnRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNjg5NDcsImV4cCI6MjAyNTk0NDk0N30.75RrOs3_kXa8VYbJPUW5mP-zIiiw7w7DYrG8jZYodOg';
-
+  user_id: string | undefined; 
   private s_client : SupabaseClient;
   constructor(private http: HttpClient) { 
     this.s_client = createClient(this.apiUrl,this.apiKey);
@@ -40,7 +41,7 @@ export class SupabaseService {
   }
 
 
-  signIn(email: string, password: string) {
+  signIn(email: string, password: string): Observable<any> {
     const headers = new HttpHeaders({
       'apikey': this.apiKey,
       'Content-Type': 'application/json'
@@ -56,5 +57,29 @@ export class SupabaseService {
 
   logout(){
     return this.s_client.auth.signOut();
+  }
+
+  insert(vectorData: any): Observable<any> {
+    if (!this.user_id) {
+      throw new Error('El usuario no está autenticado.');
+    }
+
+    const headers = new HttpHeaders({
+      'apikey': this.apiKey,
+      'Content-Type': 'application/json'
+    });
+
+
+    const vectorDataWithUserId = {
+      ...vectorData,
+      user_id: this.user_id
+    };
+
+    return this.http.post(`${this.apiUrl}/rest/v1/vectors_table`, vectorDataWithUserId, { headers });
+  }
+
+
+  setUser(user_id: string) {
+    this.user_id = user_id;
   }
 }
