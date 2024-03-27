a6086808b8202ed2f3e6b7a686ade0c41bbc4aaf
PJMonticoli <pablo_monticoli@hotmail.com>
2024-03-27 16:12:01 -0300

    modifico metodo getvectors, ahora filtra por user_id

--- helphubapp/src/app/services/supabase.service.ts
+++ helphubapp/src/app/services/supabase.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
@@ -15,16 +16,39 @@ export class SupabaseService {
   apiKey : string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpbnh2bWNveGx0YWdxamxsbnRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAzNjg5NDcsImV4cCI6MjAyNTk0NDk0N30.75RrOs3_kXa8VYbJPUW5mP-zIiiw7w7DYrG8jZYodOg';
   user_id: string | undefined; 
   private s_client : SupabaseClient;
+
+
   constructor(private http: HttpClient) { 
     this.s_client = createClient(this.apiUrl,this.apiKey);
   }
 
-  getVectors() {
+
+  getVectorsByUserId(user_id: string): Observable<any> {
     const headers = {
-      'apikey': this.apiKey
+      'apikey': this.apiKey,
     };
-    return this.http.get(`${this.apiUrl}/rest/v1/vectors_table`, { headers });
+    return this.http.get(`${this.apiUrl}/rest/v1/vectors_table?user_id=eq.${user_id}`, { headers });
   }
+
+  
+  signIn(email: string, password: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'apikey': this.apiKey,
+      'Content-Type': 'application/json'
+    });
+  
+    const body = {
+      email: email,
+      password: password
+    };
+  
+    return this.http.post(`${this.apiUrl}/auth/v1/token?grant_type=password`, body, { headers }).pipe(
+      tap((response: any) => {
+        this.setUser(response.user.id); 
+      })
+    );
+  }
+
 
   signUp(email: string, password: string) {
     const headers = new HttpHeaders({
@@ -41,26 +65,31 @@ export class SupabaseService {
   }
 
 
-  signIn(email: string, password: string): Observable<any> {
-    const headers = new HttpHeaders({
-      'apikey': this.apiKey,
-      'Content-Type': 'application/json'
-    });
-  
-    const body = {
-      email: email,
-      password: password
-    };
-  
-    return this.http.post(`${this.apiUrl}/auth/v1/token?grant_type=password`, body, { headers });
-  }
-
   logout(){
     return this.s_client.auth.signOut();
   }
 
+  // insert(vectorData: any): Observable<any> {
+  //   if (!this.user_id) {
+  //     throw new Error('El usuario no está autenticado.');
+  //   }
+
+  //   const headers = new HttpHeaders({
+  //     'apikey': this.apiKey,
+  //     'Content-Type': 'application/json'
+  //   });
+
+
+  //   const vectorDataWithUserId = {
+  //     ...vectorData,
+  //     user_id: this.user_id
+  //   };
+
+  //   return this.http.post(`${this.apiUrl}/rest/v1/vectors_table`, vectorDataWithUserId, { headers });
+  // }
   insert(vectorData: any): Observable<any> {
-    if (!this.user_id) {
+    const user_id = this.getUserId(); 
+    if (!user_id) {
       throw new Error('El usuario no está autenticado.');
     }
 
@@ -69,17 +98,19 @@ export class SupabaseService {
       'Content-Type': 'application/json'
     });
 
-
     const vectorDataWithUserId = {
       ...vectorData,
-      user_id: this.user_id
+      user_id: user_id
     };
 
     return this.http.post(`${this.apiUrl}/rest/v1/vectors_table`, vectorDataWithUserId, { headers });
   }
-
-
+  
   setUser(user_id: string) {
     this.user_id = user_id;
+  }
+  
+  getUserId(): string | undefined {
+    return this.user_id;
   }
 }
