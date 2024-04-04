104c6f19b01620f862ee0b829f80f53fef5ee259
PJMonticoli <pablo_monticoli@hotmail.com>
2024-04-04 11:42:07 -0300

    estructuro codigo y comento para mejorar legibilidad

--- helphubapp/src/app/services/supabase.service.ts
+++ helphubapp/src/app/services/supabase.service.ts
@@ -1,9 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
-
-
-import { SupabaseClient,createClient } from '@supabase/supabase-js'; // Asegúrate de importar SupabaseAuthClient
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 
@@ -14,20 +11,14 @@ import { environment } from '../../environments/environment.development';
   providedIn: 'root'
 })
 export class SupabaseService {
- 
   user_id: string | undefined; 
-  private s_client! : SupabaseClient;
+
+  constructor(private http: HttpClient) {}
 
 
-  constructor(private http: HttpClient) { 
-    //SI yo agrego esta linea en el constructor,al intentar inyectar la dependencia del serivicio SupabaseService en mi componente
-    // sidenav.component.ts, la pagina web deja de cargarse en el navegador al ejecutar en consola "ng serve -o" queda cargando y no finaliza su carga
-   //this.s_client = createClient(environment.apiUrl,environment.apiKey);
+  //CRUD USER
 
-  }
-
-
-
+  //GET usuario por id 
   getVectorsByUserId(user_id: string): Observable<any> {
     const headers = {
       'apikey': environment.apiKey,
@@ -36,7 +27,7 @@ export class SupabaseService {
     return this.http.get(`${environment.apiUrl}/rest/v1/vectors_table?user_id=eq.${user_id}`, { headers });
   }
 
-  
+  //Iniciar Sesión
   signIn(email: string, password: string): Observable<any> {
     const headers = new HttpHeaders({
       'apikey': environment.apiKey,
@@ -57,7 +48,7 @@ export class SupabaseService {
   }
 
 
-
+  // Registrarse
   signUp(email: string, password: string) {
     const headers = new HttpHeaders({
       'apikey': environment.apiKey,
@@ -72,6 +63,7 @@ export class SupabaseService {
     return this.http.post(`${environment.apiUrl}/auth/v1/signup`, body, { headers });
   }
 
+  // Cerrar Sesión
   logout(): Observable<any> {
     const headers = new HttpHeaders({
       'apikey': environment.apiKey,
@@ -86,12 +78,46 @@ export class SupabaseService {
     );
   }
 
-  
+  // Limpiar Sesión 
   private clearSession(): void {
     localStorage.removeItem('access_token');
     localStorage.removeItem('user_id');
   }
 
+  // GET - SET manejo sesión y tokens
+  
+
+setToken(token: string): void {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('access_token', token);
+  }
+}
+
+getToken(): string | null {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem('access_token');
+  }
+  return null;
+}
+
+  
+setUser(user_id: string): void {
+  this.user_id = user_id;
+  localStorage.setItem('user_id', user_id);
+}
+  
+getUserId(): string | undefined {
+    return this.user_id;
+ }
+
+
+isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  // CRUD VECTORS_TABLE
+
+  // Registrar vector
   insert(vectorData: any): Observable<any> {
     const user_id = this.getUserId(); 
     if (!user_id) {
@@ -117,74 +143,49 @@ export class SupabaseService {
     return this.http.post(`${environment.apiUrl}/rest/v1/vectors_table`, vectorDataWithUserId, { headers });
 }
 
-setToken(token: string): void {
-  if (typeof window !== 'undefined') {
-    localStorage.setItem('access_token', token);
+// Modificar vector
+modificar(vectorId: number, vectorDataUpdated: any): Observable<any> {
+  const user_id = this.getUserId(); 
+  if (!user_id) {
+    throw new Error('El usuario no está autenticado.');
   }
+  const token = this.getToken(); 
+  if (!token) {
+    throw new Error('El token de acceso no está disponible.');
+  }
+  const headers = new HttpHeaders({
+    'apikey': environment.apiKey,
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}` 
+  });
+
+  const vectorDataWithUserId = {
+    ...vectorDataUpdated,
+    user_id: user_id
+  };
+
+  return this.http.patch(`${environment.apiUrl}/rest/v1/vectors_table?id=eq.${vectorId}`, vectorDataWithUserId, { headers });
 }
 
-getToken(): string | null {
-  if (typeof window !== 'undefined') {
-    return localStorage.getItem('access_token');
+// Eliminar vector
+deleteVector(vectorId: any): Observable<any> {
+  const user_id = this.getUserId();
+  if (!user_id) {
+    throw new Error('El usuario no está autenticado.');
   }
-  return null;
+
+  const token = this.getToken();
+  if (!token) {
+    throw new Error('El token de acceso no está disponible.');
+  }
+
+  const headers = new HttpHeaders({
+    'apikey': environment.apiKey,
+    'Authorization': `Bearer ${token}`
+  });
+
+  return this.http.delete(`${environment.apiUrl}/rest/v1/vectors_table?id=eq.${vectorId}`, { headers });
 }
 
-  
-setUser(user_id: string): void {
-  this.user_id = user_id;
-  localStorage.setItem('user_id', user_id);
-}
-  
-  getUserId(): string | undefined {
-    return this.user_id;
-  }
 
-
-  modificar(vectorId: number, vectorDataUpdated: any): Observable<any> {
-    const user_id = this.getUserId(); 
-    if (!user_id) {
-      throw new Error('El usuario no está autenticado.');
-    }
-    const token = this.getToken(); 
-    if (!token) {
-      throw new Error('El token de acceso no está disponible.');
-    }
-    const headers = new HttpHeaders({
-      'apikey': environment.apiKey,
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}` 
-    });
-  
-    const vectorDataWithUserId = {
-      ...vectorDataUpdated,
-      user_id: user_id
-    };
-  
-    return this.http.patch(`${environment.apiUrl}/rest/v1/vectors_table?id=eq.${vectorId}`, vectorDataWithUserId, { headers });
-  }
-  
-  
-  deleteVector(vectorId: any): Observable<any> {
-    const user_id = this.getUserId();
-    if (!user_id) {
-      throw new Error('El usuario no está autenticado.');
-    }
-
-    const token = this.getToken();
-    if (!token) {
-      throw new Error('El token de acceso no está disponible.');
-    }
-
-    const headers = new HttpHeaders({
-      'apikey': environment.apiKey,
-      'Authorization': `Bearer ${token}`
-    });
-
-    return this.http.delete(`${environment.apiUrl}/rest/v1/vectors_table?id=eq.${vectorId}`, { headers });
-  }
-
-  isLoggedIn(): boolean {
-    return !!this.getToken();
-  }
 }
