756be05fd5caca286f2b17199184262b8299b049
PJMonticoli <pablo_monticoli@hotmail.com>
2024-04-04 08:48:08 -0300

    agrego fitlro busqueda tabla vectores

--- helphubapp/src/app/components/vectors/vectors-table/vectors-table.component.ts
+++ helphubapp/src/app/components/vectors/vectors-table/vectors-table.component.ts
@@ -8,18 +8,20 @@ import { SupabaseService } from '../../../services/supabase.service';
 import { DeleteVectorsComponent } from '../delete-vectors/delete-vectors.component';
 import { UpdateVectorsComponent } from '../update-vectors/update-vectors.component';
 import { InsertVectorsComponent } from '../insert-vectors/insert-vectors.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-vectors-table',
   standalone: true,
-  imports: [RouterLink,MatPaginatorModule,InsertVectorsComponent,UpdateVectorsComponent,DeleteVectorsComponent],
+  imports: [RouterLink,MatPaginatorModule,InsertVectorsComponent,UpdateVectorsComponent,DeleteVectorsComponent,FormsModule],
   templateUrl: './vectors-table.component.html',
   styleUrl: './vectors-table.component.css'
 })
 export class VectorsTableComponent implements OnInit {
   vectors: any[] = [];
+  filtroTexto: string = '';
   page: number = 0; 
   pageSize: number = 7; 
   constructor(private supabaseService : SupabaseService,private toastr : ToastrService){}
@@ -46,5 +48,24 @@ export class VectorsTableComponent implements OnInit {
     const startIndex = this.page * this.pageSize;
     return this.vectors.slice(startIndex, startIndex + this.pageSize);
   } 
+  
+  filtrarTabla(): void {
+    const filtro = this.normalizeString(this.filtroTexto);
+  
+    if (filtro.trim() === '') {
+      this.fetchVectorsForCurrentUser();
+    } else {
+      this.vectors = this.vectors.filter(row =>
+        this.normalizeString(row.pregunta)?.includes(filtro) || 
+        this.normalizeString(row.respuesta)?.includes(filtro)
+      );
+      this.page = 0;
+    }
+  }
+  
+  
+  normalizeString(text: string): string {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+  }
   
 }
