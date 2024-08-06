056e2c70de21b76d7a279eb684e6d5bdced842d6
PJMonticoli <pablo_monticoli@hotmail.com>
2024-08-06 16:04:58 -0300

    WIP background images y styles en general

--- helphubapp/src/app/app.component.ts
+++ helphubapp/src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,SidenavComponent],
+  imports: [RouterOutlet, RouterLink, SidenavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'helphubapp';
+  title = 'PabloDev';
 }
--- helphubapp/src/app/components/users/user-register/user-register.component.ts
+++ helphubapp/src/app/components/users/user-register/user-register.component.ts
@@ -1,33 +1,37 @@
 import { Component, Input } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { SupabaseService } from '../../../services/supabase.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-
 
 @Component({
   selector: 'app-user-register',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './user-register.component.html',
-  styleUrl: './user-register.component.css'
+  styleUrl: './user-register.component.css',
 })
 export class UserRegisterComponent {
   private subscription = new Subscription();
   formulario: FormGroup;
   fieldTextType: boolean = false;
 
-
   constructor(
     private formBuilder: FormBuilder,
-    private servicioSupabase : SupabaseService,
-    private router : Router,
-    private toastr : ToastrService
+    private servicioSupabase: SupabaseService,
+    private router: Router,
+    private toastr: ToastrService
   ) {
     this.formulario = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
@@ -36,15 +40,13 @@ export class UserRegisterComponent {
   }
   ngOnInit(): void {}
 
-
   get controlEmail(): FormControl {
     return this.formulario.controls['email'] as FormControl;
   }
 
-  get controlPassword() : FormControl{
+  get controlPassword(): FormControl {
     return this.formulario.controls['password'] as FormControl;
   }
-
 
   toggleFieldTextType() {
     this.fieldTextType = !this.fieldTextType;
@@ -54,20 +56,36 @@ export class UserRegisterComponent {
     if (this.formulario.valid) {
       const email = this.formulario.value.email;
       const password = this.formulario.value.password;
-  
+
       this.servicioSupabase.signUp(email, password).subscribe({
         next: () => {
-          this.toastr.success("Usuario registrado con éxito",'Bienvenido!',{timeOut:1500});
+          this.toastr.success(
+            '¡Registro exitoso! Por favor, revisa tu correo electrónico y confirma tu cuenta siguiendo el enlace proporcionado en el mensaje que te hemos enviado',
+            'Bienvenido!',
+            { timeOut: 2000 }
+          );
           this.router.navigate(['/user-login']);
         },
         error: (err) => {
-          if (err.status === 422 && err.error && err.error.error_code === "user_already_exists") {
-            this.toastr.error("El correo electrónico ya está en uso, por favor ingrese otro diferente.",'Error',{timeOut:1500});
+          if (
+            err.status === 422 &&
+            err.error &&
+            err.error.error_code === 'user_already_exists'
+          ) {
+            this.toastr.error(
+              'El correo electrónico ya está en uso, por favor ingrese otro diferente.',
+              'Error',
+              { timeOut: 1500 }
+            );
           } else {
-            this.toastr.error("Error al registrarse, revise y complete todos los campos!",'Error',{timeOut:1500});
+            this.toastr.error(
+              'Error al registrarse, revise y complete todos los campos!',
+              'Error',
+              { timeOut: 1500 }
+            );
           }
           console.log(err);
-        }
+        },
       });
     }
   }
